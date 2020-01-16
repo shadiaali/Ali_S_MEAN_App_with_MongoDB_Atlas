01baa1b768aea44a2b59130ec1ae7113a0bb214f
Shadia Ali <shadia@Shadias-MBP.fc.ca>
2020-01-16 10:43:59 -0500

    conn to cloud, movie model done

---
+++ models/Movie.js
@@ -0,0 +1,54 @@
+//require mongoose
+var mongoose = require('mongoose');
+
+//new schema
+//objects are specified in curly braces
+//strings can be put in arrays
+//if data is stored in a date format you can do much more query wise
+//Eg, using greater than or less than in the query.
+//You can also subtract this date from this date and it’ll give you the number, etc.
+//It is a lot more efficient than strings. 
+//you can nest arrays and objects
+
+var movieSchema = new mongoose.Schema({
+
+plot: String,
+    genres: [String],
+    runtime: Number,
+    cast: [String],
+    num_mflix_comments: Number,
+    title: String,
+    fullplot: String,
+    countries: [String],
+    released: Date,
+    directors: [String],
+    rated: String,
+    awards: {
+        wins: Number,
+        nominations: Number,
+        text: String
+    },
+    lastupdated: Date,
+    year: Number,
+    imdb: {
+        rating: Number,
+        votes: Number,
+        id: Number
+    },
+    type: String,
+    tomatoes: {
+        viewer: {
+            rating: Number,
+            numReviews: Number,
+            meter: Number
+        },
+        lastupdated: Date,
+        
+    },
+    
+    //specifying the collection just for shits and giggles
+    collection: 'movies'
+
+});
+
+module.exports = mongoose.model('Movie', movieSchema);
---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+//require the schema model
+var Movie = require('../models/Movie.js');
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+
+  //if there's an error, return it and stop execution
+  //otherwise render out the movies as json
+  //every route should have json rendered out FIRST. 
+  //Rob wants to see this for EVERY ROUTE
+  //Vue is all about json. You only want RAW json data
+  //in Vue you don't need to build out any ejs views 
+  //Vue handles all the json data because it is a View (vue) engine!
+  //no need for ejs or handlebars in Vue!
+  //comment routes out for vue 
+
+  Movie.find(function (err, movies) {
+    if (err) return next(err);
+    res.json(movies);
+  });
+
+
+  //don't need this. commenting out
+  //res.render('index', { title: 'Express' });
+
+});
+
+
+module.exports = router;
